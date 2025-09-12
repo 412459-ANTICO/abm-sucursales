d82cb3755e8ccb4f765bcbe08b0fcd052c488f6e
412459-ANTICO <412459@tecnicatura.frc.utn.edu.ar>
2025-09-12 01:03:13 -0300

    row-component eliminado

--- src/app/shared/sucursales-table/sucursales-table.component.ts
+++ src/app/shared/sucursales-table/sucursales-table.component.ts
@@ -1,11 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {Sucursal} from '../../models/sucursal';
-import {SucursalRowComponent} from '../sucursal-row/sucursal-row.component';
 
 @Component({
   selector: 'app-sucursales-table',
   imports: [
-    SucursalRowComponent
   ],
   templateUrl: './sucursales-table.component.html',
   styleUrl: './sucursales-table.component.css'
