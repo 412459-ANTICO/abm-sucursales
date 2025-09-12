caf537d9fbc159aab630d3dfd1d4e6f8d67903cb
412459-ANTICO <412459@tecnicatura.frc.utn.edu.ar>
2025-09-12 14:44:07 -0300

    pulidos botones formulario alta

--- src/app/shared/sucursal-form/sucursal-form.component.ts
+++ src/app/shared/sucursal-form/sucursal-form.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatDialogClose} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-sucursal-form',
-  imports: [],
+  imports: [
+    MatDialogClose
+  ],
   templateUrl: './sucursal-form.component.html',
   styleUrl: './sucursal-form.component.css'
 })
